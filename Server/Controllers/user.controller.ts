6eeb60e444cb4e11ac94ab91f100558daf321e3b
Marcela Cer <ramiplatero11@gmail.com>
2023-06-08 20:02:15 -0300

    adding sign up route

--- Server/Controllers/user.controller.ts
+++ Server/Controllers/user.controller.ts
@@ -1,6 +1,19 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { User } from "../entities/User";
 
+export const signUp = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
 
-export const signUp = (_req:Request,res:Response)=>{
-    res.send("works")
-}
+  try {
+    const user = new User();
+    user.username = username;
+    user.email = email;
+    user.password = password;
+    await user.save();
+    return res.status(200).json({ message: "new user created" });
+  } catch (error) {
+    if(error instanceof Error){
+        return res.status(500).json({ error: error.message });
+    }
+  }
+};
--- Server/Routes/user.routes.ts
+++ Server/Routes/user.routes.ts
@@ -1,8 +1,8 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { signUp } from "../Controllers/user.controller";
 
 const router = Router()
 
-router.get('/users',signUp)
+router.post('/signup',signUp)
 
 export default router
