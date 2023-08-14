28b345c45ffb0fec62ddb4d3d20483d74bc02ed1
Rami Platero <fabriplatero88@gmail.com>
2023-08-14 18:24:56 -0300

    feat: add filters logic to search products

--- Server/Controllers/product.controller.ts
+++ Server/Controllers/product.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Product } from "../entities/Product";
+import { AppError } from "../helpers/AppError";
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
@@ -13,7 +14,7 @@ export const createProduct = async (req: Request, res: Response) => {
       main_image,
       images,
       stock,
-      name
+      name,
     });
 
     const newProduct = await product.save();
@@ -27,4 +28,72 @@ export const createProduct = async (req: Request, res: Response) => {
   }
 };
 
+enum Filters {
+  name_asc = "name_asc",
+  name_desc = "name_desc",
+  price_asc = "price_asc",
+  price_desc = "price_desc",
+}
 
+type Params = {
+  search: string;
+  filter: Filters;
+  page: string;
+};
+
+export const getProducts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { search, filter, page } = req.query as Params;
+    let products = await Product.find();
+    if (!products) throw new AppError(404, "Products not found");
+
+    if (search) {
+      products = products.filter((product) => {
+        return product.name.toLowerCase().includes(search.toLowerCase());
+      });
+    }
+
+    if (filter) {
+      switch (filter) {
+        case Filters.name_asc:
+          products.sort((a, b) => {
+            return a.name.localeCompare(b.name);
+          });
+          break;
+        case Filters.name_desc:
+          products.sort((a, b) => {
+            return b.name.localeCompare(a.name);
+          });
+          break;
+        case Filters.price_desc:
+          products.sort((a, b) => {
+            return b.price - a.price;
+          });
+          break;
+        case Filters.price_asc:
+          products.sort((a, b) => {
+            return a.price - b.price;
+          });
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (page) {
+      const postsPerPage = 15;
+      products = products.slice(
+        (parseInt(page) - 1) * postsPerPage,
+        postsPerPage * parseInt(page)
+      );
+    }
+
+    return res.status(200).json(products);
+  } catch (error) {
+    return next(error);
+  }
+};
