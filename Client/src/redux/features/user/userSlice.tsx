e96ab9943e11baabdfc80d86ebe336ad4e65bba9
Rami Platero <fabriplatero88@gmail.com>
2023-08-14 18:12:42 -0300

    add login and logout reducers

--- Client/src/redux/features/user/userSlice.tsx
+++ Client/src/redux/features/user/userSlice.tsx
@@ -1,11 +1,21 @@
 import {createSlice} from '@reduxjs/toolkit'
+import { User } from '../../../types'
+
+const initialState = {} as User
 
 export const userSlice = createSlice({
     name: "user",
-    initialState: {},
+    initialState,
     reducers: {
-
+        setCredentials: (state,action)=>{
+            return state = action.payload
+        },
+        logOut: (state,_action)=>{
+            return state = initialState
+        }
     }
 })
+
+export const {setCredentials,logOut} = userSlice.actions
 
 export default userSlice.reducer
