832e23aa1286305562d36444960ff018cfbac510
Rami Platero <fabriplatero88@gmail.com>
2023-08-14 18:16:39 -0300

    fixed authContext import

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { UserContextProvider } from "./context/UserContext.tsx";
+import { AuthContextProvider } from "./context/AuthContext.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <UserContextProvider>
+    <AuthContextProvider>
       <App />
-    </UserContextProvider>
+    </AuthContextProvider>
   </Provider>
 );
