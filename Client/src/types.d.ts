03fe55c6e6a6f3729de8f02e2b0f4b3451c0ba9d
Rami Platero <fabriplatero88@gmail.com>
2023-08-14 18:17:45 -0300

    add product types

--- Client/src/types.d.ts
+++ Client/src/types.d.ts
@@ -1,8 +1,32 @@
-export interface IUser {
+export type User = {
     id: number;
     username: string;
     email: string;
     password: string;
     createdAt: Date,
     updatedAt: Date
-  }
+}
+
+export enum Category {
+  GPU = "Graphics Card",
+  CPU = "CPU",
+  Monitor = "Monitor",
+}
+
+export type ProductImage = {
+  thumbnail: string;
+  xl: string;
+}
+
+export type Product = {
+  id: number;
+  name: string;
+  category: Category;
+  description: string;
+  price: number;
+  stock: number;
+  main_image: ProductImage;
+  images: ProductImage[];
+  createdAt: Date;
+  updatedAt: Date
+}
