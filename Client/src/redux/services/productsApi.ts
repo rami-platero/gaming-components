37ff4343a916634ec5c8d155eec938cc9adefdb8
Rami Platero <fabriplatero88@gmail.com>
2023-08-14 18:15:19 -0300

    create products API + search query

---
+++ Client/src/redux/services/productsApi.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Product } from "../../types";
+
+export type Params = {
+  search: string;
+  page: string;
+  filter: string;
+};
+
+export const productsApi = createApi({
+  reducerPath: "productsAPI",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:4000/api/",
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    getProducts: builder.query<Product[], Params>({
+      query: (params) =>
+        `products/?search=${params.search}&filter=${params.filter}&page=${params.page}`,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productsApi;
--- Client/src/redux/store.ts
+++ Client/src/redux/store.ts
@@ -1,15 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './features/user/userSlice'
+import { userApi } from "./services/userApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import userReducer from "./features/user/userSlice";
+import productReducer from "./features/products/productsSlice";
+import { productsApi } from "./services/productsApi";
 
 const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    [userApi.reducerPath]: userApi.reducer,
+    products: productReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware, productsApi.middleware),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
