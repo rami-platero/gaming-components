96e9c36e92268b77d76a085738c90c255394efce
Rami Platero <ramiplatero11@gmail.com>
2023-08-26 14:26:12 -0300

    create mutation endpoints for login/signup

--- Client/src/redux/services/userApi.ts
+++ Client/src/redux/services/userApi.ts
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { User } from "../../types";
+import { SignUpSchema } from "../../Schemas/SignUpSchema";
+import { LoginSchema } from "../../Schemas/LoginSchema";
 
-const backend_endpoint: string = "http://localhost:4000/api/";
+const backend_endpoint: string = "http://localhost:4000/api/auth";
 
 export const userApi = createApi({
   reducerPath: "userAPI",
@@ -10,10 +12,24 @@ export const userApi = createApi({
     credentials: "include",
   }),
   endpoints: (builder) => ({
-    getUser: builder.query<User[], null>({
+    getUser: builder.query<User, null>({
       query: () => "getUser",
+    }),
+    signUp: builder.mutation<User, SignUpSchema>({
+      query: (body) => ({
+        url: "signup",
+        method: "POST",
+        body,
+      }),
+    }),
+    login: builder.mutation<User, LoginSchema>({
+      query: (body) => ({
+        url: "login",
+        method: "POST",
+        body,
+      }),
     }),
   }),
 });
 
-export const { useGetUserQuery } = userApi;
+export const { useGetUserQuery, useSignUpMutation, useLoginMutation } = userApi;
