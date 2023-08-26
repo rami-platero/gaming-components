8ba064251d3b38ba71fa65422ff3a19de6966c86
Rami Platero <ramiplatero11@gmail.com>
2023-08-26 14:31:41 -0300

    set up Persistor component

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,11 +4,17 @@ import "./index.scss";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+export const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </Provider>
+  </PersistGate>
 );
