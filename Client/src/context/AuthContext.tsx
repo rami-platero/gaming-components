6c58e0192f530eb6edbecc0ef92005c1dab280b5
Rami Platero <ramiplatero11@gmail.com>
2023-08-26 14:36:48 -0300

    set up global authentication with redux and add logout function

--- Client/src/context/AuthContext.tsx
+++ Client/src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { useGetUserQuery } from "../redux/services/userApi";
-import { useAppDispatch } from "../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { setCredentials,logOut } from "../redux/features/user/userSlice";
 
 export interface ContextProps {
@@ -12,39 +12,55 @@ interface IContext {
   logout: () => void;
   isAuthenticated: boolean;
   loading: boolean;
+  authenticate: ()=>void
 }
 
 export const authContext = createContext<IContext>({
   logout: () => {},
   isAuthenticated: false,
   loading: false,
+  authenticate: ()=>{}
 });
 
 export const AuthContextProvider = ({ children }: ContextProps) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
-  const { data, isSuccess } = useGetUserQuery(null);
-
+  const user = useAppSelector(state=>state.user)
+  const { data, isSuccess } = useGetUserQuery(null, {
+    skip: user!==null
+  });
   const dispatch = useAppDispatch();
+
+  const authenticate = ()=>{
+    setIsAuthenticated(true)
+  }
 
   const logout = async () => {
     Cookies.remove("token");
-    setIsAuthenticated(false);
     dispatch(logOut(null))
+    setIsAuthenticated(false);
+    window.location.reload()
   };
 
   useEffect(()=>{
-    if(data && isSuccess){
+    if(user) {
       setIsAuthenticated(true)
       setLoading(false)
-      dispatch(setCredentials(data))
+      return
     }
-    setLoading(false)
-
+    if(data && isSuccess){
+      dispatch(setCredentials(data))
+      setIsAuthenticated(true)
+      setLoading(false)
+    } else if (!data && !isSuccess){
+      setIsAuthenticated(false)
+      setLoading(false)
+    } 
   },[data])
 
+
   return (
-    <authContext.Provider value={{ logout, isAuthenticated, loading }}>
+    <authContext.Provider value={{ logout, isAuthenticated, loading, authenticate }}>
       {children}
     </authContext.Provider>
   );
