2ae8c159f08ceb98ab1e26a7ede52a4b30873944
Rami Platero <ramiplatero11@gmail.com>
2023-08-26 14:31:07 -0300

    add Roles enum type

--- Client/src/types.d.ts
+++ Client/src/types.d.ts
@@ -1,8 +1,14 @@
+export enum Roles {
+  user = "User",
+  admin = "Admin",
+}
+
 export type User = {
     id: number;
     username: string;
     email: string;
     password: string;
+    roles: Roles[];
     createdAt: Date,
     updatedAt: Date
 }
