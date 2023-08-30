d4d5dcf6e8581095aa06e7cfe8ff9525eb779002
Rami Platero <ramiplatero11@gmail.com>
2023-08-30 16:31:05 -0300

    change cors origin based on NODE_ENV variable

--- Server/app.ts
+++ Server/app.ts
@@ -2,23 +2,29 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import router from "./Routes/user.routes";
-import productRouter from './Routes/product.routes'
-import commentRouter from './Routes/comment.routes'
+import productRouter from "./Routes/product.routes";
+import commentRouter from "./Routes/comment.routes";
 import passport from "passport";
 import session from "express-session";
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
-const PassportSetup = require('./config/passport-setup')
-import cookieParser from 'cookie-parser'
+const PassportSetup = require("./config/passport-setup");
+import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 app.use(morgan("dev"));
-app.use(cookieParser())
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5173"
+        : process.env.CLIENT_BASE_URL,
+    credentials: true,
+  })
+);
+console.log("NODE ENV",process.env.NODE_ENV);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -29,9 +35,9 @@ app.use(
   })
 );
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
-app.use("/", router,productRouter,commentRouter);
-app.use(errorHandler)
+app.use("/", router, productRouter, commentRouter);
+app.use(errorHandler);
 
 export default app;
