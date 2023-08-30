149cb3252ccfb97031a37045eb1081302ad1a3fc
Rami Platero <ramiplatero11@gmail.com>
2023-08-30 16:24:03 -0300

    change api url

--- Client/src/redux/services/productsApi.ts
+++ Client/src/redux/services/productsApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {  Products } from "../../types/products";
+import config from "../../config/config";
 
 export type Params = {
   search: string;
@@ -10,13 +11,13 @@ export type Params = {
 export const productsApi = createApi({
   reducerPath: "productsAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000/api/",
+    baseUrl: config.API_BASE_URL,
     credentials: "include",
   }),
   endpoints: (builder) => ({
     getProducts: builder.query<Products, Params>({
       query: (params) =>
-        `products/?search=${params.search}&filter=${params.filter}&page=${params.page}`,
+        `/products/?search=${params.search}&filter=${params.filter}&page=${params.page}`,
     }),
   }),
 });
--- Client/src/redux/services/userApi.ts
+++ Client/src/redux/services/userApi.ts
@@ -2,29 +2,28 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { User } from "../../types";
 import { SignUpSchema } from "../../Schemas/SignUpSchema";
 import { LoginSchema } from "../../Schemas/LoginSchema";
-
-const backend_endpoint: string = "http://localhost:4000/api/auth";
+import config from "../../config/config";
 
 export const userApi = createApi({
   reducerPath: "userAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: backend_endpoint,
+    baseUrl: `${config.API_BASE_URL}/auth`,
     credentials: "include",
   }),
   endpoints: (builder) => ({
     getUser: builder.query<User, null>({
-      query: () => "getUser",
+      query: () => "/getUser",
     }),
     signUp: builder.mutation<User, SignUpSchema>({
       query: (body) => ({
-        url: "signup",
+        url: "/signup",
         method: "POST",
         body,
       }),
     }),
     login: builder.mutation<User, LoginSchema>({
       query: (body) => ({
-        url: "login",
+        url: "/login",
         method: "POST",
         body,
       }),
