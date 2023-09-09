56af8e35faef82ac6b5b650ef4ed58c5ea8ef253
Rami Platero <ramiplatero11@gmail.com>
2023-09-09 18:46:07 -0300

    inject auth api endpoints

---
+++ Client/src/redux/services/authApiSlice.ts
@@ -0,0 +1,38 @@
+import { LoginSchema, SignUpSchema } from "../../schemas/auth.schema";
+import { User } from "../../types";
+import { apiSlice } from "./apiSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUser: builder.query<User, null>({
+      query: () => "/auth/getUser",
+    }),
+    signUp: builder.mutation<User, SignUpSchema>({
+      query: (body) => ({
+        url: "/auth/signup",
+        method: "POST",
+        body,
+      }),
+    }),
+    login: builder.mutation<User, LoginSchema>({
+      query: (body) => ({
+        url: "/auth/login",
+        method: "POST",
+        body,
+      }),
+    }),
+    logOut: builder.mutation<null, null>({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetUserQuery,
+  useSignUpMutation,
+  useLoginMutation,
+  useLogOutMutation,
+} = authApiSlice;
