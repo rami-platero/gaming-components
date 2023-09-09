e08f1a8baa0af0f25a3d69a0317a3e38011ca0d8
Rami Platero <ramiplatero11@gmail.com>
2023-09-09 18:54:51 -0300

    add new reducers and handle auth accessToken

---
+++ Client/src/redux/features/user/authSlice.ts
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Roles, User } from "../../../types";
+import jwtDecode from "jwt-decode";
+import Cookies from "js-cookie";
+
+export type decodedJWT = {
+  user: {
+    roles: Roles[];
+  };
+};
+
+export type AuthState = {
+  auth: {
+    token: string | null;
+    user: User | null;
+    isAuthenticated: boolean;
+    loading: boolean;
+  };
+};
+
+const initialState: AuthState["auth"] = {
+  user: null,
+  token: null,
+  isAuthenticated: !!Cookies.get("logged_in"),
+  loading: true,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      const { user, accessToken } = action.payload;
+
+      // decode accessToken to get roles
+      const decoded: decodedJWT = jwtDecode(accessToken);
+
+      const newUser: User = { ...user, roles: decoded.user.roles };
+      state.user = newUser;
+      state.token = accessToken;
+      state.isAuthenticated = true;
+      state.loading = false
+      return;
+    },
+    logOut: (state, _action) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+      state.loading = false
+      return;
+    },
+    handleLoading: (state,action) => {
+      state.loading = action.payload
+      return
+    }
+  },
+});
+
+export const { setCredentials, logOut, handleLoading } = authSlice.actions;
+
+export default authSlice.reducer;
