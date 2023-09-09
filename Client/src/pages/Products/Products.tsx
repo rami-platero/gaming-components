1d0be04a841df84345d826e9d7c22a8fd189cf43
Rami Platero <ramiplatero11@gmail.com>
2023-09-09 19:04:00 -0300

    add breadcrumb and products skeleton on loading

--- Client/src/pages/Products/Products.tsx
+++ Client/src/pages/Products/Products.tsx
@@ -1,99 +1,67 @@
-import { useGetProductsQuery } from "../../redux/services/productsApi";
-import { useSearchParams } from "react-router-dom";
-import { useEffect } from "react";
 import styles from "./products.module.scss";
-import ProductItem from "./ProductItem";
-import Search from "./Filters/Search";
-import SortBy from "./Filters/SortBy";
-import Pagination from "./Filters/Pagination";
-import { IoMdClose } from "react-icons/io";
-import {ToastContainer} from 'react-toastify'
-import useToast from "../../hooks/useToast";
+import ProductCard from "../../components/ProductCard";
+import Search from "../../components/Filters/Search";
+import SortBy from "../../components/Filters/SortBy";
+import Pagination from "../../components/Filters/Pagination";
+import { ToastContainer } from "react-toastify";
+import Filters from "../../components/Filters";
+import { useAppSelector } from "../../redux/hooks";
+import { useContext } from "react";
+import { productsContext } from "../../context/ProductsContext";
+import Breadcrumb from "../../components/Breadcrumb";
+import ProductCardSkeleton from "../../components/Skeleton/ProductCardSkeleton";
 
 const Products = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const search = searchParams.get("search") || "";
-  const filter = searchParams.get("filter") || "";
-  const page = searchParams.get("page") || "1";
-
-  const {notifyError} = useToast()
-
-  const removeCurrentSearch = () => {
-    setSearchParams((prevSearchParams) => {
-      const newSearchParams = new URLSearchParams(prevSearchParams);
-      newSearchParams.delete("search");
-      return newSearchParams;
-    });
-  };
-
-  useEffect(() => {
-    setSearchParams((prevSearchParams) => {
-      const newSearchParams = new URLSearchParams(prevSearchParams);
-      newSearchParams.set("page", page);
-      return newSearchParams;
-    });
-  }, []);
-
-  const { data, isError } = useGetProductsQuery({
-    search,
-    filter,
-    page,
-  });
-
-  useEffect(()=>{
-    isError && notifyError("Internal Server Error.")
-  },[isError])
+  const data = useAppSelector((state) => state.products);
+  const { isLoading } = useContext(productsContext);
 
   return (
     <>
-      <ToastContainer limit={1}/>
+      <ToastContainer limit={1} />
       <main className={styles.products}>
-      <div className={styles.products__filters}>
-        <h2>Filters</h2>
-        <div className={styles.products__filters__wrapper}></div>
-      </div>
-      <div className={styles.products__content}>
-        <h1>Products</h1>
+        <Breadcrumb />
+        <div className={styles.products__container}>
+          {/* Main Filters */}
+          <Filters filters={data?.filters} />
 
-        {/* Filters */}
-        <section className={styles.products__content__mainFilters}>
-          <Search setSearchParams={setSearchParams} />
-          <SortBy setSearchParams={setSearchParams} filter={filter} />
-        </section>
+          <div className={styles.products__container__content}>
+            <h1>Products</h1>
 
-        {search && (
-          <div className={styles.products__content__currentFilter}>
-            <p>{search}</p>
-            <button>
-              <IoMdClose onClick={removeCurrentSearch} />
-            </button>
+            {/* Top Filters */}
+            <section
+              className={styles.products__container__content__mainFilters}
+            >
+              <Search />
+              <SortBy />
+            </section>
+
+            {/* Products */}
+            <section className={styles.products__container__content__wrapper}>
+              {!!isLoading && <ProductCardSkeleton/>}
+              {!!data?.products?.length &&
+                data?.products.map((product) => {
+                  return <ProductCard key={product.id} product={product} />;
+                })}
+              {!isLoading && !data?.products?.length && (
+                <div
+                  className={
+                    styles.products__container__content__wrapper__notFound
+                  }
+                >
+                  <div>0 results found</div>
+                  <h3>Couldn't find any products. Try another search.</h3>
+                </div>
+              )}
+            </section>
+
+            {/* Pagination */}
+            {!!data?.pages_amount && data?.pages_amount > 1 && (
+              <Pagination pages_amount={data?.pages_amount} />
+            )}
           </div>
-        )}
-
-        {/* Products */}
-        <section className={styles.products__content__wrapper}>
-          {data?.products?.length ? (
-            data.products.map((product) => {
-              return <ProductItem key={product.id} product={product} />;
-            })
-          ) : (
-            <h2>No data</h2>
-          )}
-        </section>
-
-        {/* Pagination */}
-        {!!data?.pages_amount && data?.pages_amount > 1 && (
-          <Pagination
-            pages_amount={data.pages_amount}
-            setSearchParams={setSearchParams}
-            currentPage={page}
-          />
-        )}
-      </div>
-    </main>
+        </div>
+      </main>
     </>
-   
   );
 };
 
