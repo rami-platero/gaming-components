c75be015cac1b8f2f1f21afdb2ff2dec65d684a2
Rami Platero <ramiplatero11@gmail.com>
2023-09-09 18:51:47 -0300

    add products filters types

--- Client/src/types/products.d.ts
+++ Client/src/types/products.d.ts
@@ -11,20 +11,55 @@ export type ProductImage = {
   xl: string;
 };
 
-export type Product = {
+export type TProduct = {
   id: number;
   name: string;
+  slug: string;
   category: Category;
   description: string;
   price: number;
   stock: number;
+  brand: string
   main_image: ProductImage;
   images: ProductImage[];
   createdAt: Date;
   updatedAt: Date;
 };
 
-export type Products = {
-    products: Product[],
-    pages_amount: number
+export enum Rating {
+  One = 1,
+  Two = 2,
+  Three = 3,
+  Four = 4,
+  Five = 5,
 }
+
+export type TPriceRange = {
+  min: number;
+  max: number;
+};
+
+export type TFilters = {
+  priceRange: TPriceRange;
+  brands: string[];
+  rating: Rating;
+};
+
+export type TCurrentFilters = {
+  search: string;
+  filter: string;
+  page: string;
+  priceRange: {
+    max: string,
+    min: string
+  }
+  brands: string;
+  rating: Rating;
+}
+
+export type TProducts = {
+  products: TProduct[];
+  pages_amount: number;
+  filters?: TFilters;
+  currentFilters: TCurrentFilters;
+};
