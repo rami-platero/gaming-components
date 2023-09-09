3b95e9ab57130061949ff4e5e47c16f11627bf08
Rami Platero <ramiplatero11@gmail.com>
2023-09-09 18:51:08 -0300

    add new routes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,12 +3,14 @@ import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import AuthRoutes from "./components/auth-routes";
 import Layout from "./components/layout";
-import HomePage from "./pages/home-page/home-page";
+import HomePage from "./pages/HomePage/home-page";
 import Products from "./pages/Products/Products";
-import NotFound from "./pages/not-found/not-found";
+import NotFound from "./pages/NotFound/not-found";
 import ProtectedRoutes from "./components/protected-routes";
 import Profile from "./pages/Profile/Profile";
 import { Roles } from "./types.d";
+import { ProductsContextProvider } from "./context/ProductsContext";
+import ProductPage from "./pages/Product/ProductPage";
 
 const App = () => {
   return (
@@ -17,13 +19,28 @@ const App = () => {
         {/* Layout */}
         <Route element={<Layout />}>
           <Route element={<HomePage />} path="/" />
-          <Route element={<Products />} path="/products" />
+
+          <Route
+            element={
+              <ProductsContextProvider>
+                <Products />
+              </ProductsContextProvider>
+            }
+            path="/products"
+          />
+          <Route
+            element={
+              <ProductsContextProvider>
+                <Products />
+              </ProductsContextProvider>
+            }
+            path="/products/:category"
+          />
+          <Route element={<ProductPage/>} path="/products/:category/:slug" />
           <Route element={<NotFound />} path="/*" />
           <Route element={<ProtectedRoutes allowedRoles={[Roles.user]} />}>
             <Route element={<Profile />} path="/profile" />
           </Route>
-
-          
 
           {/* Auth Routes */}
           <Route element={<AuthRoutes />}>
@@ -32,8 +49,6 @@ const App = () => {
           </Route>
         </Route>
         {/* Auth Routes */}
-
-
 
         {/* Admin Routes */}
 
