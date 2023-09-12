209c91a8fed6554f863ebe1b153fb89bc948f07f
Rami Platero <ramiplatero11@gmail.com>
2023-09-11 21:39:22 -0300

    add hook to track screen width

---
+++ Client/src/hooks/useScreenWidth.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+const useScreenWidth =() =>{
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return screenWidth;
+}
+
+export default useScreenWidth;
