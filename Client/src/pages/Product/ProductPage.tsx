f17873c04309dbded84093aa47fccb41757700a3
Rami Platero <ramiplatero11@gmail.com>
2023-09-11 21:24:15 -0300

    add skeleton conditional rendering

--- Client/src/pages/Product/ProductPage.tsx
+++ Client/src/pages/Product/ProductPage.tsx
@@ -5,15 +5,17 @@ import ProductInfo from "../../components/SingleProduct/ProductInfo";
 import ProductSpecs from "../../components/SingleProduct/ProductSpecs";
 import ProductComments from "../../components/SingleProduct/ProductComments";
 import Breadcrumb from "../../components/Breadcrumb";
+import SingleProductSkeleton from "../../components/Skeleton/SingleProduct/SingleProductSkeleton";
 
 const ProductPage = () => {
   const { slug } = useParams();
 
-  const { isError, data } = useGetSingleProductQuery(slug || "");
+  const { isError, data, isFetching } = useGetSingleProductQuery(slug || "");
 
   return (
     <main className={styles.product}>
       {isError && <h2>Product does not exist!</h2>}
+      {!!isFetching && !data && <SingleProductSkeleton/>}
       {data && (
         <>
           <Breadcrumb current={data.name} />
