03470b56d0610d7e231a7df421f6a69cddce452b
Rami Platero <ramiplatero11@gmail.com>
2023-09-29 03:10:08 -0300

    remove Profile component

--- Client/src/components/layout.tsx
+++ Client/src/components/layout.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Footer from "./Footer/Footer"
-import Navigation from "./Navigation/Navigation"
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer/Footer";
+import Navigation from "./Navigation/Navigation";
 
 const Layout = () => {
   return (
     <>
-    <Navigation/>
-        <Outlet/>
-    <Footer/>
+      <Navigation />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
