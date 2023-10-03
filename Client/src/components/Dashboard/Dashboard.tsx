40b00d95ee600df1152874acc9bde5d407e61641
Rami Platero <ramiplatero11@gmail.com>
2023-10-03 01:55:34 -0300

    ui: render orders/avatar skeletons when loading

--- Client/src/components/Dashboard/Dashboard.tsx
+++ Client/src/components/Dashboard/Dashboard.tsx
@@ -5,14 +5,19 @@ import Avatar from "../../assets/default_pfp.png";
 import { BsCartX } from "react-icons/bs";
 import Image from "../../assets/rtx3070.png";
 import { useAppSelector } from "../../redux/hooks";
+import { useGetOrdersQuery } from "../../redux/services/ordersApi";
+import OrdersTableSkeleton from "../Skeleton/Dashboard/OrdersTableSkeleton";
+import ImageWithLoader from '../Image'
+import config from "../../config/config";
 
 const Dashboard = () => {
   const user = useAppSelector((state) => state.auth.user);
   const cart = useAppSelector((state) => state.cart);
+  const { data: orders, isFetching } = useGetOrdersQuery(null);
 
   return (
     <div className={styles.dashboard}>
-      <h1>Hi, Ramiro!</h1>
+      <h1>Hi, {user?.username}</h1>
       <h2>Welcome to the dashboard.</h2>
 
       <div className={styles.dashboard__widgets}>
@@ -22,7 +27,8 @@ const Dashboard = () => {
             <Link to={"/dashboard/orders"}>View all</Link>
           </div>
           <div className={styles.dashboard__widgets__orders__wrapper}>
-            <OrdersTable />
+            {isFetching && <OrdersTableSkeleton length={2} />}
+            {!isFetching && !!orders && <OrdersTable orders={orders} />}
           </div>
         </div>
 
@@ -32,7 +38,9 @@ const Dashboard = () => {
             <Link to={"/dashboard/settings"}>Account settings</Link>
           </div>
           <div className={styles.dashboard__widgets__profile__wrapper}>
-            <img src={Avatar} />
+            <div className={styles.dashboard__widgets__profile__wrapper__imgContainer}>
+            {user?.avatar ? <ImageWithLoader src={`${config.API_BASE_URL}/avatar/${user.avatar}`}/> : <img src={Avatar} />}
+            </div>
             <h3>{user?.username}</h3>
             <h4>{user?.email}</h4>
           </div>
@@ -65,7 +73,7 @@ const Dashboard = () => {
               <BsCartX />
               <h4>Your cart is empty!</h4>
               <h5>You haven't added any items to your cart yet.</h5>
-              <button>Shop now!</button>
+              <Link to={"/products"}>Shop now!</Link>
             </div>
           ) : (
             <div className={styles.dashboard__widgets__cart__wrapper}>
