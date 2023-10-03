39d425637d92d164a0f9b73acd3b61e19d8ce75f
Rami Platero <ramiplatero11@gmail.com>
2023-10-03 01:59:30 -0300

    ui: render orders skeleton when loading

--- Client/src/components/Dashboard/Orders.tsx
+++ Client/src/components/Dashboard/Orders.tsx
@@ -1,12 +1,17 @@
+import { useGetOrdersQuery } from "../../redux/services/ordersApi";
+import OrdersTableSkeleton from "../Skeleton/Dashboard/OrdersTableSkeleton";
 import styles from "./orders.module.scss";
 import OrdersTable from "./Widgets/OrdersTable";
 
 const Orders = () => {
+  const { data: orders, isFetching } = useGetOrdersQuery(null);
+
   return (
     <div className={styles.orders}>
       <h1>Orders</h1>
       <p>Manage and view a detailed list of your orders history.</p>
-      <OrdersTable />
+      {!isFetching && !!orders && <OrdersTable orders={orders} />}
+      {isFetching && <OrdersTableSkeleton length={5}/>}
     </div>
   );
 };
