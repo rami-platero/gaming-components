359a0013c3d12806faee6bb9b39de212026d8dd3
Rami Platero <ramiplatero11@gmail.com>
2023-10-03 02:25:21 -0300

    config: add oauth2 api google enpoint

--- Client/src/config/config.ts
+++ Client/src/config/config.ts
@@ -3,6 +3,8 @@ const config = {
     import.meta.env.MODE === "development"
       ? "http://localhost:4000/api"
       : import.meta.env.VITE_REACT_BASE_API_URL,
+  API_GOOGLE_URL: import.meta.env.MODE === "development" ? "http://localhost:4000/auth/google"
+  : import.meta.env.VITE_REACT_API_GOOGLE_URL,
 };
 
 export default config;
