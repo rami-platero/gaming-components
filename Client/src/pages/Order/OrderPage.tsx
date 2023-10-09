8a87fa04e88dec6877ccba37e873badadc557344
Rami Platero <ramiplatero11@gmail.com>
2023-10-09 16:58:54 -0300

    feat: show summary and table skeleton on loading

--- Client/src/pages/Order/OrderPage.tsx
+++ Client/src/pages/Order/OrderPage.tsx
@@ -3,10 +3,12 @@ import { BsArrowLeftShort } from "react-icons/bs";
 import { Link, useParams } from "react-router-dom";
 import { useGetOrderQuery } from "../../redux/services/ordersApi";
 import OrderTable from "../../components/Dashboard/Widgets/OrderTable";
+import OrdersTableSkeleton from "../../components/Skeleton/Dashboard/OrdersTableSkeleton";
+import SummarySkeleton from "../../components/Skeleton/SummarySkeleton";
 
 const OrderPage = () => {
   const { id } = useParams();
-  const { data: order } = useGetOrderQuery(id || "");
+  const { data: order, isLoading } = useGetOrderQuery(id || "");
 
   const date = order && new Date(order?.createdAt).toLocaleDateString("en-UK");
 
@@ -17,23 +19,30 @@ const OrderPage = () => {
       </Link>
       <h1>Order Number #{order?.id}</h1>
       <div className={styles.order__container}>
-        <OrderTable order={order!} />
-
-        <div className={styles.order__container__summary}>
-          <h2>Order summary</h2>
-          <div>
-            <h4>Order created</h4>
-            <h4>{date}</h4>
+        {!isLoading ? (
+          <OrderTable order={order!} />
+        ) : (
+          <OrdersTableSkeleton columns={4} rows={7} maxWidth={"700px"} />
+        )}
+        {!isLoading ? (
+          <div className={styles.order__container__summary}>
+            <h2>Order summary</h2>
+            <div>
+              <h4>Order created</h4>
+              <h4>{date}</h4>
+            </div>
+            <div>
+              <h4>Subtotal</h4>
+              <h4>${order?.subtotal.toLocaleString("en-US")}.00</h4>
+            </div>
+            <div>
+              <h4>Total</h4>
+              <h4>${order?.total.toLocaleString("en-US")}.00</h4>
+            </div>
           </div>
-          <div>
-            <h4>Subtotal</h4>
-            <h4>${order?.subtotal.toLocaleString("en-US")}.00</h4>
-          </div>
-          <div>
-            <h4>Total</h4>
-            <h4>${order?.total.toLocaleString("en-US")}.00</h4>
-          </div>
-        </div>
+        ) : (
+          <SummarySkeleton maxWidth={"350px"}/>
+        )}
       </div>
     </div>
   );
