7d0417a847de080fb757df93ac8ba6acb282ae6d
Rami Platero <ramiplatero11@gmail.com>
2023-10-09 17:00:16 -0300

    feat: get limit param and transform orders array reponse

--- Client/src/redux/services/ordersApi.ts
+++ Client/src/redux/services/ordersApi.ts
@@ -3,10 +3,16 @@ import { apiSlice } from "./apiSlice";
 
 export const ordersApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        getOrders: builder.query<Order[],null>({
+        getOrders: builder.query<Order[],number | null>({
             query: () => ({
                 url: "/orders"
-            })
+            }),
+            transformResponse(baseQueryReturnValue: Order[], _meta, limit) {
+                if(limit){
+                    return baseQueryReturnValue.slice(0,2)
+                } 
+                return baseQueryReturnValue
+            },
         }),
         getOrder: builder.query<Order,string>({
             query: (id) => ({
