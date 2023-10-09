1975011732d9d00fc0e5c5034e23c5fc52926361
Rami Platero <ramiplatero11@gmail.com>
2023-10-09 17:01:36 -0300

    feat: get new props to set cols and rows of skeleton table, and props to get CSSProperties

--- Client/src/components/Skeleton/Dashboard/OrdersTableSkeleton.tsx
+++ Client/src/components/Skeleton/Dashboard/OrdersTableSkeleton.tsx
@@ -1,33 +1,39 @@
 import styles from "./ordersTableSkeleton.module.scss";
+import { CSSProperties } from "react";
 
-const OrdersTableSkeleton = ({length}: {length: number}) => {
-  const array = [];
-  for (let i = 0; i < length; i++) {
-    array.push(i)
+interface Props extends CSSProperties {
+  rows: number;
+  columns: number;
+}
+
+const OrdersTableSkeleton = ({ rows, columns, ...style }: Props) => {
+  const rowsArray: number[] = [];
+  for (let i = 0; i < rows; i++) {
+    rowsArray.push(i);
+  }
+  const colsArray: number[] = [];
+  for (let i = 0; i < columns; i++) {
+    colsArray.push(i);
   }
 
   return (
-    <div className={styles.container}>
-        <div className={styles.container__header}>
-          <h3></h3>
-          <h3></h3>
-          <h3></h3>
-          <h3></h3>
-          <h3></h3>
-        </div>
-        <div className={styles.container__body}>
-          {array?.map((item) => {
-            return (
-              <div className={styles.container__body__row} key={item}>
-                <div className={styles.orderID}></div>
-                <div className={styles.date}></div>
-                <div className={styles.itemsAmount}></div>
-                <div className={styles.total}></div>
-                <div className={styles.status}></div>
-              </div>
-            );
-          })}
-        </div>
+    <div className={styles.container} style={style}>
+      <div className={styles.container__header}>
+        {colsArray.map((item) => {
+          return <h3 key={item}></h3>;
+        })}
+      </div>
+      <div className={styles.container__body}>
+        {rowsArray?.map((item) => {
+          return (
+            <div className={styles.container__body__row} key={item}>
+              {colsArray.map((item) => {
+                return <div key={item}></div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
