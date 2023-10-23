25847f13575ae783358f67feb1e50e8cfd1d752c
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:04:30 -0300

    feat: add reviews routes (get,post,edit)

--- Server/src/routes/review.routes.ts
+++ Server/src/routes/review.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middlewares/authentication";
-import { deleteReview, editReview, getReviewsFromProduct, postReview } from "../controllers/review.controller";
+import { deleteReview, editReview, getReviews, getReviewsFromProduct, postReview } from "../controllers/review.controller";
 
 const router = Router();
 
-router.get("/api/reviews/:id", getReviewsFromProduct);
-router.post("/api/review", /* isAuthorized, */ postReview);
+/* router.get("/api/reviews/:id", getReviewsFromProduct); */
+router.get("/api/reviews/:id", getReviews)
+router.post("/api/reviews/:id", isAuthenticated, postReview);
 router.delete("/api/reviews/:id", isAuthenticated, deleteReview);
-router.put("/api/reviews/:id", editReview);
+router.put("/api/reviews/:id", isAuthenticated, editReview);
 
 export default router;
