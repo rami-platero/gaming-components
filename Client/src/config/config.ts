9b64d51a4198eb37f93fc8979ebc9a46883aa2d7
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:12:01 -0300

    update config: add CDN_URL env variable

--- Client/src/config/config.ts
+++ Client/src/config/config.ts
@@ -3,8 +3,11 @@ const config = {
     import.meta.env.MODE === "development"
       ? "http://localhost:4000/api"
       : import.meta.env.VITE_REACT_BASE_API_URL,
-  API_GOOGLE_URL: import.meta.env.MODE === "development" ? "http://localhost:4000/auth/google"
-  : import.meta.env.VITE_REACT_API_GOOGLE_URL,
+  API_GOOGLE_URL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:4000/auth/google"
+      : import.meta.env.VITE_REACT_API_GOOGLE_URL,
+  CDN_URL: import.meta.env.VITE_REACT_CDN_URL,
 };
 
 export default config;
