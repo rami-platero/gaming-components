9f6e8a05b58700438d2a5f16f34f302bab79bc45
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:20:22 -0300

    refactor: add the ToastContainer in the global layout

--- Client/src/components/layout.tsx
+++ Client/src/components/layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import Navigation from "./Navigation/Navigation";
+import { ToastContainer } from "react-toastify";
 
 const Layout = () => {
   return (
     <>
+      <ToastContainer limit={3} />
       <Navigation />
       <Outlet />
       <Footer />
