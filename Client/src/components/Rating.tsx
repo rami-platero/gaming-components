1f8657fc44edf8c258b3108dc2dfe2280dd4f867
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:24:28 -0300

    feat: add global product rating component

--- Client/src/components/Rating.tsx
+++ Client/src/components/Rating.tsx
@@ -1,5 +1,5 @@
-import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 import styles from "./rating.module.scss";
+import RatingStars from "./RatingStars";
 
 type Props = {
   rating: {
@@ -9,8 +9,6 @@ type Props = {
 };
 
 const Rating = ({rating }: Props) => {
-  const ratingArray = Array.from({ length: 5 }, (_, i) => i);
-
   const handleScroll = () => {
 
   }
@@ -18,17 +16,7 @@ const Rating = ({rating }: Props) => {
   return (
     <div className={styles.rating}>
       <div className={styles.rating__stars} onClick={handleScroll}>
-        {ratingArray.map((element) => {
-          // display filled stars if I have already reviewed the product
-          if (rating.avg >= element + 1) {
-            return (
-                <AiFillStar key={element}/>
-            );
-          }
-          return (
-              <AiOutlineStar key={element}/>
-          );
-        })}
+        <RatingStars rating={rating.avg}/>
       </div>
       <p>{rating.avg} ({rating.amount} reviews)</p>
     </div>
