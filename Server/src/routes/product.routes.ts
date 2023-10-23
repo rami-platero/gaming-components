25b2c34c05e67752edc71459bacbb682468e144e
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:03:45 -0300

    feat: add multer upload for images on products creation post route

--- Server/src/routes/product.routes.ts
+++ Server/src/routes/product.routes.ts
@@ -8,10 +8,14 @@ import {
   getProductsWithCategory,
   updateBrand,
 } from "../controllers/product.controller";
+import multer from "multer";
 
 const router = Router();
 
-router.post("/api/product", /* isAuthenticated, */ createProduct);
+const storage = multer.memoryStorage()
+const upload = multer({storage: storage})
+
+router.post("/api/product", /* isAuthenticated, */upload.array('files', 4), createProduct);
 router.get("/api/products", getProducts);
 router.get("/api/products/:category", getProductsWithCategory);
 router.route("/api/products/:id").patch(updateBrand).delete(deleteProduct);
