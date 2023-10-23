3c455080724c8afc6c7d411c73ff61e8fa8a1ff8
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:07:34 -0300

    feat: add getReviews, postReview function in review controller, fix editReviews function

--- Server/src/controllers/review.controller.ts
+++ Server/src/controllers/review.controller.ts
@@ -4,6 +4,7 @@ import { Product } from "../entities/Product";
 import { User } from "../entities/User";
 import { AppError } from "../helpers/AppError";
 import { AccessToken } from "../../types";
+import { getFileURL } from "../utils/s3";
 
 export const getReviewsFromProduct = async (req: Request, res: Response) => {
   try {
@@ -29,29 +30,61 @@ export const getReviewsFromProduct = async (req: Request, res: Response) => {
   }
 };
 
-export const postReview = async (req: Request, res: Response) => {
+export const postReview = async (req: Request, res: Response, next:NextFunction) => {
   try {
-    /* const {user} = req */
-    const { body, product_id, rating } = req.body;
+    const decodedUser = res.locals.user as AccessToken["user"];
+    const { id } = req.params;
+    const { body, rating } = req.body;
 
     const product = (await Product.findOne({
       where: {
-        id: product_id,
+        id: Number(id),
       },
     })) as Product;
 
-    const user = (await User.findOne({ where: { id: 29 } })) as User;
+    const exists = await Review.findOne({
+      where: {
+        user: {
+          id: decodedUser.id,
+        },
+        product: {
+          id: Number(id)
+        }
+      },
+    });
 
-    const review = Review.create({ body, user, product, edited: false, rating });
+    if (exists)
+      throw new AppError(
+        400,
+        JSON.stringify({ message: "You have already reviewed this product!" })
+      );
 
-    const newReview = await review.save();
+    const user = (await User.findOne({
+      where: { id: decodedUser.id },
+    })) as User;
 
-    return res.status(200).json({ message: "Success", newReview });
+    const review = Review.create({
+      body,
+      user,
+      product,
+      edited: false,
+      rating,
+    });
+
+    await review.save();
+    const { product: xd, user: reviewUser, ...rest } = review;
+    const responseReview = {
+      ...rest,
+      product_id: review.product.id,
+      avatar: null,
+      username: review.user.username,
+      user_id: review.user.id
+    };
+
+    return res.status(200).json(responseReview);
   } catch (error) {
-    if (error instanceof Error) {
-      return res.status(500).json({ error: error.message });
-    }
-    return res.status(500).json();
+    console.log(error);
+    return next(error)
   }
 };
 
@@ -73,7 +106,7 @@ export const deleteReview = async (
     if (!review) {
       throw new AppError(
         404,
-        JSON.stringify({ review: "Review does not exist" })
+        JSON.stringify({ message: "Review does not exist" })
       );
     }
 
@@ -85,38 +118,84 @@ export const deleteReview = async (
       .execute();
 
     if (deleteResult.affected) {
-      return res.status(204).json({ message: "Review deleted" });
+      return res.sendStatus(204)
     }
     throw new AppError(
       401,
-      JSON.stringify({ review: "You can't delete this review" })
+      JSON.stringify({ message: "You can't delete this review" })
     );
   } catch (error) {
+    console.log(error);
     return next(error);
   }
 };
 
-export const editReview = async (req: Request, res: Response) => {
+export const editReview = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const {user} = res.locals as AccessToken
+    const {id} = req.params
     const { body } = req.body;
     if (!body) {
       throw new Error("You must provide a text body to edit a review");
     }
 
-    const editedReview = await Review.createQueryBuilder()
+    await Review.createQueryBuilder()
       .update(Review)
       .set({ body, edited: true })
-      .where("id =:id", { id: req.params.id })
-      .returning("*")
+      .where("id =:id", { id: Number(id) })
+      .andWhere("user_id =:user_id", {user_id: user.id})
       .execute();
 
-    return res
-      .status(200)
-      .json({ message: "Success!", editedReview: editedReview.raw });
+    return res.status(200).json({ message: "Success!"});
   } catch (error) {
-    if (error instanceof Error) {
-      return res.status(500).json({ error: error.message });
+    console.log(error);
+    return next(error)
+  }
+};
+
+export const getReviews = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { offset, date } = req.query;
+    const { id } = req.params;
+
+    console.log(new Date(Number(date)));
+
+    let reviews = (await Review.createQueryBuilder("review")
+      .select([
+        "review.*",
+        "user.username AS username",
+        "user.id AS user_id",
+        "user.avatar AS avatar",
+      ])
+      .innerJoin("review.user", "user")
+      .where("review.product_id=:product_id", { product_id: id })
+      .andWhere("review.createdAt < :date", {date: new Date(Number(date)).toISOString()})
+      .offset(Number(offset))
+      .limit(5)
+      .orderBy("review.createdAt", "DESC")
+      .execute()) as (Review & { avatar: string | null; username: string, user_id: string })[];
+
+    for (let review of reviews) {
+      if (review.avatar) {
+        const signedUrl = await getFileURL(review.avatar);
+        review.avatar = signedUrl;
+      }
     }
-    return res.status(500).json({ error });
+
+    const totalReviewsQuery = Review.createQueryBuilder()
+      .select("COUNT(*)", "count")
+      .where("product_id = :product_id", { product_id: id });
+
+    const [{ count }] = await totalReviewsQuery.getRawMany();
+    const hasNextPage = count > Number(offset) + reviews.length;
+
+    return res.status(200).json({ hasNextPage, reviews });
+  } catch (error) {
+    console.log(error);
+    return next(error);
   }
 };
