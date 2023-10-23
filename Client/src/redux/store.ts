c738eb34ecb3ee5dd54f6e776f71e8863dd0a40f
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:10:42 -0300

    feat: add error middleware logger and cartApi + reviewsApi middlewares

--- Client/src/redux/store.ts
+++ Client/src/redux/store.ts
@@ -4,8 +4,29 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import userReducer from "./features/user/authSlice";
 import productReducer from "./features/products/productsSlice";
 import { productsApi } from "./services/productsApi";
-import cartReducer from './features/cart/cartSlice'
+import cartReducer from "./features/cart/cartSlice";
 import { cartApi } from "./services/cartApi";
+import { reviewsApi } from "./services/reviewsApi";
+import { isRejectedWithValue } from "@reduxjs/toolkit";
+import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
+import useToast from "../hooks/useToast";
+import { isCustomError } from "../utils/checkErrors";
+const { notifyError } = useToast();
+
+export const rtkQueryErrorLogger: Middleware =
+  (_api: MiddlewareAPI) => (next) => (action) => {
+    // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
+    if (
+      isRejectedWithValue(action) &&
+      action.meta.arg.endpointName !== "getUser" &&
+      !isCustomError(action.payload)
+    ) {
+      console.log(action);
+      notifyError("Internal server error");
+    }
+
+    return next(action);
+  };
 
 const store = configureStore({
   reducer: {
@@ -14,13 +35,16 @@ const store = configureStore({
     products: productReducer,
     [productsApi.reducerPath]: productsApi.reducer,
     cart: cartReducer,
-    [cartApi.reducerPath]: cartApi.reducer
+    [cartApi.reducerPath]: cartApi.reducer,
+    [reviewsApi.reducerPath]: reviewsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(
       apiSlice.middleware,
       productsApi.middleware,
-      cartApi.middleware
+      cartApi.middleware,
+      reviewsApi.middleware,
+      rtkQueryErrorLogger
     ),
 });
 
