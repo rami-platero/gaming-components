d200f2c2c08bc68efb9f6f619dd29b84fe87a4ad
Rami Platero <ramiplatero11@gmail.com>
2023-10-22 22:00:20 -0300

    feat: add functions to check api error messages

--- Client/src/utils/checkErrors.ts
+++ Client/src/utils/checkErrors.ts
@@ -1,13 +1,41 @@
 export const checkError = (error: any) => {
-    if (error && "status" in error) {
-      return (
-        error.status === 500 ||
-        error.status === "TIMEOUT_ERROR" ||
-        error.status === "FETCH_ERROR" ||
-        error.status === "PARSING_ERROR" ||
-        error.status === "CUSTOM_ERROR"
-      );
-    } else {
-      return false;
-    }
+  if (error && "status" in error) {
+    return (
+      error.status === 500 ||
+      error.status === "TIMEOUT_ERROR" ||
+      error.status === "FETCH_ERROR" ||
+      error.status === "PARSING_ERROR" ||
+      error.status === "CUSTOM_ERROR"
+    );
+  } else {
+    return false;
+  }
+};
+
+type ErrorWithMessage = {
+  status: number;
+  data: {
+    message: string;
   };
+};
+
+export function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "status" in error &&
+    "data" in error &&
+    !!error.data &&
+    typeof error.data === "object" &&
+    "message" in error.data
+  );
+}
+
+export function isCustomError(error: unknown): boolean {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "data" in error &&
+    typeof error.data === "object"
+  );
+}
