a850ea8f2893e6edcd1839fed567e4f5d90954e9
Rami Platero <ramiplatero11@gmail.com>
2023-11-04 20:40:40 -0300

    refactor: remove unused import

--- Server/src/middlewares/errorHandler.ts
+++ Server/src/middlewares/errorHandler.ts
@@ -1,4 +1,4 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { AppError } from "../helpers/AppError";
 
 interface MyParams {
