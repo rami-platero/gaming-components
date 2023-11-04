1d05213ca774d63e64f0fee21d0212cc16cf4e31
Rami Platero <ramiplatero11@gmail.com>
2023-11-04 20:36:24 -0300

    feat: add changePassword schema

--- Server/src/schemas/auth.schema.ts
+++ Server/src/schemas/auth.schema.ts
@@ -21,3 +21,19 @@ export const signUpSchema = z.object({
 });
 
 export type SignUpSchema = z.infer<typeof signUpSchema>;
+
+export const changePasswordSchema = z
+  .object({
+    oldPassword: z.string().nonempty("This field is required"),
+    newPassword: z
+      .string()
+      .nonempty("This field is required")
+      .min(8, "Password must be at least 8 characters long."),
+    repeatPassword: z.string().nonempty("This field is required"),
+  })
+  .refine((data) => data.newPassword === data.repeatPassword, {
+    message: "Password doesn't match",
+    path: ["repeatPassword"],
+  });
+
+export type ChangePasswordSchema = z.infer<typeof changePasswordSchema>;
