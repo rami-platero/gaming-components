578f312dec09afd53f0a8120a040bd6f18526b20
Rami Platero <ramiplatero11@gmail.com>
2023-11-04 21:05:54 -0300

    feat: add footer sitemap links

--- Client/src/components/Footer/Footer.tsx
+++ Client/src/components/Footer/Footer.tsx
@@ -7,39 +7,36 @@ import {AiOutlineGooglePlus} from 'react-icons/ai'
 import {AiOutlineMail} from 'react-icons/ai'
 
 const Footer = () => {
+
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__content}>
         <div className={styles.footer__content__col}>
           <h3>Gaming Components</h3>
-          <hr />
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe pariatur deleniti labore fugit consequatur obcaecati voluptatem quas explicabo facilis ut et doloremque quidem ad dolorem atque illo, odio dicta.
           </p>
         </div>
         <div className={styles.footer__content__col}>
           <h3>Sitemap</h3>
-          <hr />
           <div className={styles.footer__content__col__links}>
             <Link to="/">Home</Link>
             <Link to="/contact">Contact us</Link>
             <Link to="/about">About us</Link>
-            <Link to="/register">Get Started</Link>
+            {/* <Link to="/register">Get Started</Link> */}
           </div>
         </div>
         <div className={styles.footer__content__col}>
           <h3>Products</h3>
-          <hr />
           <div className={styles.footer__content__col__links}>
-            <Link to="/">GPUs</Link>
-            <Link to="/">Processors</Link>
-            <Link to="/">Motherboards</Link>
-            <Link to="/">Monitors</Link>
+            <Link to="/products/GPUs">GPUs</Link>
+            <Link to="/products/CPUs">Processors</Link>
+            <Link to="/products/Motherboards">Motherboards</Link>
+            <Link to="/products/Monitors">Monitors</Link>
           </div>
         </div>
         <div className={styles.footer__content__col}>
           <h3>Help</h3>
-          <hr />
           <div className={styles.footer__content__col__links}>
             <Link to="/">Support</Link>
             <Link to="/">Terms and Use</Link>
@@ -48,7 +45,6 @@ const Footer = () => {
         </div>
         <div className={styles.footer__content__col}>
           <h3>Contact</h3>
-          <hr />
           <a href="mailto:gamingcomponents@gmail.com" className={styles.footer__content__col__mail}>
             <AiOutlineMail/>
             <p>Mail us at</p>
