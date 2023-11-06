5578fcede0de0e34de2fd522ad28d6b514b19305
Rami Platero <ramiplatero11@gmail.com>
2023-11-05 21:55:54 -0300

    feat: handle unauthorized errors

--- Client/src/components/Cart/CartSummary.tsx
+++ Client/src/components/Cart/CartSummary.tsx
@@ -3,33 +3,44 @@ import { selectCartTotal } from "../../redux/features/cart/cartSlice";
 import { useAppSelector } from "../../redux/hooks";
 import styles from "./cartSummary.module.scss";
 import { useCreateCheckoutSessionMutation } from "../../redux/services/paymentApi";
-import Loader from '../../assets/Loader.svg'
+import Loader from "../../assets/WhiteLoader.svg";
+import { isErrorWithStatus } from "../../utils/checkErrors";
+import useToast from "../../hooks/useToast";
 
 const CartSummary = () => {
-  const total = useAppSelector(selectCartTotal)
-  const navigate = useNavigate()
+  const total = useAppSelector(selectCartTotal);
+  const navigate = useNavigate();
   const handleContinue = () => {
-    navigate("/products")
-  }
+    navigate("/products");
+  };
+  const { notifyError } = useToast();
 
-  const [checkout, {isLoading}] = useCreateCheckoutSessionMutation()
+  const [checkout, { isLoading }] = useCreateCheckoutSessionMutation();
 
   const handleCheckout = async () => {
-      checkout(null)
-  }
+    try {
+      await checkout(null).unwrap();
+    } catch (error) {
+      if (isErrorWithStatus(error) && error.originalStatus === 401) {
+        notifyError("You must login to continue with the checkout.");
+      }
+    }
+  };
 
   return (
     <div className={styles.summary}>
       <div className={styles.summary__subtotal}>
         <h4>Subtotal</h4>
-        <h5>${total.toLocaleString('en-US')}.00</h5>
+        <h5>${total.toLocaleString("en-US")}.00</h5>
       </div>
       <hr />
       <div className={styles.summary__total}>
         <h3>Total</h3>
-        <h3>${total.toLocaleString('en-US')}.00</h3>
+        <h3>${total.toLocaleString("en-US")}.00</h3>
       </div>
-      <button onClick={handleCheckout} disabled={isLoading}>{!isLoading? "Checkout": <img src={Loader} />}</button>
+      <button onClick={handleCheckout} disabled={isLoading}>
+        {!isLoading ? "Checkout" : <img src={Loader} />}
+      </button>
       <button onClick={handleContinue}>Continue Shopping</button>
     </div>
   );
--- Client/src/components/Modals/AddReviewModal.tsx
+++ Client/src/components/Modals/AddReviewModal.tsx
@@ -5,7 +5,7 @@ import { usePostReviewMutation } from "../../redux/services/reviewsApi";
 import { useForm } from "react-hook-form";
 import Loader from "../../assets/WhiteLoader.svg";
 import useToast from "../../hooks/useToast";
-import { isErrorWithMessage } from "../../utils/checkErrors";
+import { isErrorWithMessage, isErrorWithStatus } from "../../utils/checkErrors";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ReviewFormSchema, reviewFormSchema } from "../../schemas/reviewSchema";
 import { useParams } from "react-router-dom";
@@ -14,131 +14,133 @@ import { INITIAL_ATOM, uiAtom } from "../Modals";
 import useClickOutside from "../../hooks/useClickOutside";
 
 type Params = {
-  id: number
-}
+  id: number;
+};
 
-const AddReviewModal = ({id}:Params) => {
-    const [rating, setRating] = useState(0);
-    const [hovering, setHovering] = useState<number>(-1);
-    const [addReview, { isLoading }] = usePostReviewMutation();
-    const { notifyTemporalSuccess, notifyError } = useToast();
-    const {slug} = useParams()
-    const {
-      register,
-      handleSubmit,
-      formState: { errors, isSubmitting },
-    } = useForm<ReviewFormSchema>({
-      resolver: zodResolver(reviewFormSchema),
-    });
+const AddReviewModal = ({ id }: Params) => {
+  const [rating, setRating] = useState(0);
+  const [hovering, setHovering] = useState<number>(-1);
+  const [addReview, { isLoading }] = usePostReviewMutation();
+  const { notifyTemporalSuccess, notifyError } = useToast();
+  const { slug } = useParams();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ReviewFormSchema>({
+    resolver: zodResolver(reviewFormSchema),
+  });
 
-    const handleClose = () => {
-      if(!isLoading && !isSubmitting){
-        setUI(INITIAL_ATOM)
+  const handleClose = () => {
+    if (!isLoading && !isSubmitting) {
+      setUI(INITIAL_ATOM);
+    }
+  };
+
+  const setUI = useSetAtom(uiAtom);
+  const ref = useRef<HTMLDivElement>(null);
+  useClickOutside(ref, handleClose);
+
+  const ratingArray = Array.from({ length: 5 }, (_, i) => {
+    return i + 1 <= rating || i + 1 <= hovering ? (
+      <AiFillStar
+        className={`${styles.fillStar} ${
+          hovering >= i + 1 && hovering !== -1 ? styles.fillStar__hovered : ""
+        }`}
+        onMouseOver={() => {
+          setHovering(i + 1);
+        }}
+        onMouseOut={() => {
+          setHovering(-1);
+        }}
+        onClick={() => {
+          setRating(i + 1);
+        }}
+      />
+    ) : (
+      <AiFillStar
+        className={`${styles.star}`}
+        onClick={() => {
+          setRating(i + 1);
+        }}
+        onMouseOver={() => {
+          setHovering(i + 1);
+        }}
+        onMouseOut={() => {
+          setHovering(-1);
+        }}
+      />
+    );
+  });
+
+  const onSubmit = async (data: ReviewFormSchema) => {
+    try {
+      await addReview({
+        id,
+        review: {
+          body: data.body,
+          rating,
+        },
+        slug: slug || "",
+      }).unwrap();
+      notifyTemporalSuccess("Review added");
+      handleClose();
+    } catch (error) {
+      if (isErrorWithStatus(error) && error.originalStatus === 401) {
+        notifyError("You must login to review this product.");
+      }
+      if (isErrorWithMessage(error)) {
+        notifyError(error.data.message);
       }
     }
+  };
 
-    const setUI = useSetAtom(uiAtom)
-    const ref = useRef<HTMLDivElement>(null)
-    useClickOutside(ref,handleClose)
-
-
-    const ratingArray = Array.from({ length: 5 }, (_, i) => {
-      return i + 1 <= rating || i + 1 <= hovering ? (
-        <AiFillStar
-          className={`${styles.fillStar} ${
-            hovering >= i + 1 && hovering !== -1 ? styles.fillStar__hovered : ""
-          }`}
-          onMouseOver={() => {
-            setHovering(i + 1);
-          }}
-          onMouseOut={() => {
-            setHovering(-1);
-          }}
-          onClick={() => {
-            setRating(i + 1);
-          }}
-        />
-      ) : (
-        <AiFillStar
-          className={`${styles.star}`}
-          onClick={() => {
-            setRating(i + 1);
-          }}
-          onMouseOver={() => {
-            setHovering(i + 1);
-          }}
-          onMouseOut={() => {
-            setHovering(-1);
-          }}
-        />
-      );
-    });
-
-    const onSubmit = async (data: ReviewFormSchema) => {
-      try {
-        await addReview({
-          id,
-          review: {
-            body: data.body,
-            rating,
-          },
-          slug: slug || ""
-        }).unwrap();
-        notifyTemporalSuccess("Review added");
-        handleClose()
-      } catch (error) {
-        if (isErrorWithMessage(error)) {
-          notifyError(error.data.message);
-        }
-      }
-    };
-
-    return (
-      <div className={styles.modalBackground}>
-        <div
-          className={styles.modal}
-          ref={ref}
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          <button className={styles.modal__closeBtn} onClick={handleClose}>
-            <AiOutlineClose />
+  return (
+    <div className={styles.modalBackground}>
+      <div
+        className={styles.modal}
+        ref={ref}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <button className={styles.modal__closeBtn} onClick={handleClose}>
+          <AiOutlineClose />
+        </button>
+        <h2>Leave a review</h2>
+        <hr />
+        <form className={styles.modal__form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.modal__form__inputBox}>
+            <label htmlFor="">How would you rate this product</label>
+            <div className={styles.modal__form__inputBox__ratingStars}>
+              {ratingArray.map((star, index) => {
+                return <Fragment key={index}>{star}</Fragment>;
+              })}
+            </div>
+          </div>
+          <div className={styles.modal__form__inputBox}>
+            <label htmlFor="">Your review</label>
+            <textarea
+              className={`${styles.modal__form__inputBox__body} ${
+                errors.body ? styles.modal__form__inputBox__inputError : ""
+              }`}
+              {...register("body")}
+              autoFocus
+              spellCheck={false}
+            ></textarea>
+            {errors.body?.message ? (
+              <p className={styles.modal__form__inputBox__errorMessage}>
+                {errors.body.message}
+              </p>
+            ) : null}
+          </div>
+          <button type="submit" disabled={isLoading || isSubmitting}>
+            {isLoading ? <img src={Loader} /> : "Submit"}
           </button>
-          <h2>Leave a review</h2>
-          <hr />
-          <form
-            className={styles.modal__form}
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className={styles.modal__form__inputBox}>
-              <label htmlFor="">How would you rate this product</label>
-              <div className={styles.modal__form__inputBox__ratingStars}>
-                {ratingArray.map((star, index) => {
-                  return <Fragment key={index}>{star}</Fragment>;
-                })}
-              </div>
-            </div>
-            <div className={styles.modal__form__inputBox}>
-              <label htmlFor="">Your review</label>
-              <textarea
-                className={`${styles.modal__form__inputBox__body} ${
-                  errors.body ? styles.modal__form__inputBox__inputError : ""
-                }`}
-                {...register("body")}
-                autoFocus
-                spellCheck={false}
-              ></textarea>
-              {errors.body?.message ? <p className={styles.modal__form__inputBox__errorMessage}>{errors.body.message}</p> : null}
-            </div>
-            <button type="submit" disabled={isLoading || isSubmitting}>
-              {isLoading ? <img src={Loader} /> : "Submit"}
-            </button>
-          </form>
-        </div>
+        </form>
       </div>
-    );
-  }
-
+    </div>
+  );
+};
 
 export default AddReviewModal;
