00033c383eeda4ba379df2ea4e2e5b4379febbd3
rami-platero <ramiplatero11@gmail.com>
2025-12-17 09:05:00 -0300

    feat(types): add vite env types

--- Client/src/vite-env.d.ts
+++ Client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REACT_BASE_API_URL: string;
+  readonly VITE_REACT_API_GOOGLE_URL: string;
+  readonly VITE_REACT_CDN_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
